c365656ce45e26863f025a02579856b1550483af
Wilson Syring <wilson.syring@gmail.com>
2022-12-07 11:24:20 -0600

    up to the delete_exercises

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,4 +1,5 @@
 USE codeup_test_db;
+Truncate albums;
 # INSERT INTO albums (artist, name, release_date, sales, genre)
 # VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock');
 #
@@ -16,10 +17,22 @@ USE codeup_test_db;
 
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock'),
-       ('Micheal Jackson', 'Thriller', 1982, 50.2,'Pop'),
-       ('AC/DC', 'Back in Black',1980, 30.1, 'Country rock'),
-       ('Eagles','Hotel California',1976, 31.8, 'Soft rock'),
-       ('Shania Twain', 'Come on Over', 1997, 30.4,'Country'),
-       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club', 1967, 18.3,'Rock'),
-       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8,'Progressive rock'),
-       ('Pink Floyd', 'The Wall', 1979, 18.9,'Progressive rock');
+       ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop'),
+       ('AC/DC', 'Back in Black', 1980, 30.1, 'Country rock'),
+       ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock'),
+       ('Shania Twain', 'Come on Over', 1997, 30.4, 'Country'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club', 1967, 18.3, 'Rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive rock'),
+       ('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B'),
+       ('Eagles', 'Their Greatest Hits', 1976, 41.2, 'Country rock'),
+       ('Fleetwood Mac', 'Rumours', 1977, 29.3, 'Soft rock'),
+       ('Bee Gees', 'Saturday Night Fever', 1977, 22.1, 'Disco'),
+       ('Meat Loaf', 'Bat Out of Hell', 1977, 22, 'Hard rock'),
+       ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Alternative rock'),
+       ('Various artists', 'Dirty Dancing', 1987, 24.1, 'Pop'),
+       ('Celine Dion', 'Falling into You', 1996, 21.1, 'Pop'),
+       ('Adele', '21', 1996, 27.1, 'Pop'),
+       ('The Beatles', '1', 2000, 23.2, 'Rock'),
+       ('Metallica', 'Metallica', 2000, 23.2, 'Heavy metal'),
+       ('Celine Dion', 'Let''s Talk about Love', 1997, 19.5, 'Pop');
--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,5 @@ SELECT * FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+SELECT name FROM albums WHERE sales < 20;
+SELECT name FROM albums WHERE genre = 'Rock';
