36b2a803854facaabf479e0f3ac5f9a93fba62ef
Wilson Syring <wilson.syring@gmail.com>
2022-12-08 13:54:56 -0600

    completed funcions_exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,81 @@
+USE employees;
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT emp_no, first_name, gender
+FROM employees
+WHERE (first_name IN ('Irena') OR first_name IN ('Vidya') OR first_name IN ('Maya'))
+  AND gender = 'M';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE 'E%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irene', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irene', 'Vidya', 'Maya')
+ORDER BY last_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR first_name LIKE '%E'
+ORDER BY emp_no ASC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E'
+LIMIT 10;
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+LIMIT 10;
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+  AND YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY hire_date DESC
+LIMIT 10;
+
+SELECT DATEDIFF(CURDATE(), hire_date)
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+  AND YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY hire_date DESC
+LIMIT 20;
+
+
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,10 +1,17 @@
 USE codeup_test_db;
 SELECT name AS 'All albums in your table'
 FROM albums;
-UPDATE albums set sales = sales * 10;
+UPDATE albums
+set sales = sales * 10;
 SELECT name AS 'All albums released before 1980'
 FROM albums
 WHERE release_date < 1980;
-UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
-SELECT name AS 'All albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
-UPDATE albums SET artist = 'Peter Jackson' WHERE id = 3;
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+SELECT name AS 'All albums by Michael Jackson'
+FROM albums
+WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE id = 3;
